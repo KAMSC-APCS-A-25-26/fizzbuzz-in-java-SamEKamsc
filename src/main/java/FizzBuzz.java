3f611abaa73785d08b551592136311255e8bd3b2
SamEKamsc <03samuel.edmonds@kamsc.org>
2025-09-15 13:37:05 -0400

    Update FizzBuzz.java
    Fixed the code so that the for loop goes from 1 to 100 instead of 0 to 98 smh

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -9,7 +9,7 @@ public class FizzBuzz {
      * Output should be 100 whitespace-separated tokens (spaces or newlines are fine).
      */
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 1; i < 101; i++) {
             if (i % 3 == 0 & i % 5 == 0) {
                 System.out.println("Fizz Buzz");
             }
