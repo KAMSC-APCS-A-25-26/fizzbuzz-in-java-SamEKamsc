ec84d74d1f62e7a363eaa30a681193d04f1d45fb
SamEKamsc <03samuel.edmonds@kamsc.org>
2025-09-15 13:38:59 -0400

    Update FizzBuzz.java
    Changed code so that it outputs "Fizzbuzz" instead of "Fizz Buzz"

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -11,7 +11,7 @@ public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i < 101; i++) {
             if (i % 3 == 0 & i % 5 == 0) {
-                System.out.println("Fizz Buzz");
+                System.out.println("FizzBuzz");
             }
             else if (i % 3 == 0) {
                 System.out.println("Fizz");
